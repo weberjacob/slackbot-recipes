a26349ccdf90c1e1dfe2409e381031452923174e
jacobweber <jacobaweber@gmail.com>
2020-02-09 23:11:31 -0600

    now

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 
 const { App } = require('@slack/bolt');
+const express = require("express");
+const app = express();
+
+const port = 5000;
+
+// Body parser
+app.use(express.urlencoded({ extended: false }));
+
+// Listen on port 5000
+app.listen(port, () => {
+  console.log(`Server is booming on port 5000
+Visit http://localhost:5000`);
+});
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
