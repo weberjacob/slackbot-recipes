cb53d0dc8ee261b46235f5ea6dd1a39f8b2c9f71
jacobweber <jacobaweber@gmail.com>
2020-02-09 23:28:17 -0600

    .

--- index.js
+++ index.js
@@ -3,21 +3,23 @@ const { App } = require('@slack/bolt');
 const express = require("express");
 const expressapp = express();
 
-const port = 5000;
+// const port = 3000;
 
-// Body parser
-expressapp.use(express.urlencoded({ extended: false }));
+// // Body parser
+// app.use(express.urlencoded({ extended: false }));
 
-// Listen on port 5000
-expressapp.listen(port, () => {
-  console.log(`Server is booming on port 5000
-Visit http://localhost:5000`);
-});
+// // Listen on port 5000
+// app.listen(port, () => {
+//   console.log(`Server is booming on port 5000
+// Visit http://localhost:3000`);
+// });
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
+
+app.use(express.urlencoded({ extended: false }));
 
 // Listen for a slash command invocation
 app.command('/recipe', ({ ack, payload, context }) => {
