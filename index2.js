6e3b7d4f39402ec13a541e982a423117d26b140e
jacobweber <jacobaweber@gmail.com>
2020-02-09 23:35:05 -0600

    .

--- index2.js
+++ index2.js
@@ -11,3 +11,22 @@ app.listen(port, () => {
   console.log(`Server is booming on port 5000
 Visit http://localhost:5000`);
 });
+
+// Home route
+app.get("/", (req, res) => {
+  res.send("Welcome to a basic express App");
+});
+
+// Mock APIs
+app.get("/users", (req, res) => {
+  res.json([
+    { name: "William", location: "Abu Dhabi" },
+    { name: "Chris", location: "Vegas" }
+  ]);
+});
+
+app.post("/user", (req, res) => {
+  const { name, location } = req.body;
+
+  res.send({ status: "User created", name, location });
+});
