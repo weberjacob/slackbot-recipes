1ea14ca75e58517072df5d22324821d47c0090e9
jacobweber <jacobaweber@gmail.com>
2020-02-09 23:05:54 -0600

    Adding now

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+const port = 5000;
+
+// Body parser
+app.use(express.urlencoded({ extended: false }));
+
+// Listen on port 5000
+app.listen(port, () => {
+  console.log(`Server is booming on port 5000
+Visit http://localhost:5000`);
+});
